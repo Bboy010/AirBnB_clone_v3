85fd0a56b1f47af958979a0dcbbb24ba357ea9e7
Bboy010 <hkoffianderson@gmail.com>
2023-05-31 07:55:39 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,6 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
@@ -20,20 +18,6 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 def teardown(self):
     """Removes the current SQLAlchemy Session"""
     return storage.close()
-
-
-@app.errorhandler(404)
-def error(e):
-    """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
-
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
 
 
 if __name__ == '__main__':
