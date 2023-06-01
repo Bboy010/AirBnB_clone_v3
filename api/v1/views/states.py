1a901c999fa6663a9868619fa705a718dbd7b325
8srael <kouassiisrael36@gmail.com>
2023-06-01 01:44:15 -0700

    Task 6, updates api/v1/views/states.py again

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -42,7 +42,7 @@ def create_state():
         abort(400, 'Missing name')
     state = State(**request.get_json())
     state.save()
-    return jsonify(state.to_dict())
+    return jsonify(state.to_dict()), 201
 
 
 @app_views.route('states/<state_id>', methods=['PUT'], strict_slashes=False)
